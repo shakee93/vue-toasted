ca73ae3124c957b9536ea98fe4f47972128a9d1d
Shakeeb Sadikeen <shakee.zats@gmail.com>
2017-03-29 10:56:21 +0530

    updated bundling options

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path')
 var webpack = require('webpack')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   entry: './src/index.js',
